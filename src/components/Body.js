92ca589d858e084f098a64542d789ece65887482
SAMEERAHMEDS <ahmedsameersr@gmail.com>
2023-03-23 14:33:03 +0530

    initialized files with some work(code) done on foundation of our app

---
+++ src/components/Body.js
@@ -0,0 +1,73 @@
+import { restaurantList } from "../config";
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
+function filterData(searchText, restaurants) {
+  const resultData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
+  );
+  return resultData;
+}
+
+const Body = () => {
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0487446&lng=77.5923297&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  if (!allRestaurants) return null;
+
+  return allRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            //e.target.value -> Whatever we type in input box
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-List">
+        {filteredRestaurants.length === 0 ? (
+          <h1>Match Not Found</h1>
+        ) : (
+          filteredRestaurants.map((restaurant) => {
+            return (
+              <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+            );
+          })
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Body;
